b5dea8c857e64447d2d1fd4792019bfdeb6349a4
ivanpesicc <junioragdd@gmail.com>
2024-11-27 07:53:29 +0100

    Dodavanje importa u login i home komponente, izmena teksta na  home stranici

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule, JsonPipe, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor, JsonPipe, CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
