311fde3d69f856fdf6a82552b46284507f52f4bd
ivanpesicc <junioragdd@gmail.com>
2024-11-27 01:02:01 +0100

    Dodata rutiranja za login i signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +12,14 @@ export const routes: Routes = [
     {
         path: 'about',
         component: AboutComponent
+     },
+     {
+        path: 'login',
+        component: LoginComponent
+     },
+     {
+        path: 'signup',
+        component: SignupComponent
      }
 ];
+
